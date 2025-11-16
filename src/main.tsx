909f17fa082bd1631a07b170bc4a4eb35a206a9b
Nidhi Gupta <gnidhi58@gmail.com>
2025-11-16 17:29:38 +0530

    docs: enhance application entry point documentation

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,16 @@
+/**
+ * Application Entry Point
+ * 
+ * This is where our React application starts. Think of this as the ignition key
+ * that starts the entire engine. When the browser loads this file, it:
+ * 
+ * 1. Finds the HTML element with id="root" (defined in index.html)
+ * 2. Creates a React root and attaches our App component to it
+ * 3. Wraps everything in StrictMode for better development experience
+ * 
+ * StrictMode helps catch potential problems during development by running
+ * components twice and checking for deprecated APIs.
+ */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
